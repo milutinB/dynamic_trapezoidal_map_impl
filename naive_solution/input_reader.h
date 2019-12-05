0fbaf6078d378c55c8705111011b28bed5b909b3
Milutin Brankovic <milutin.brankovic3@gmail.com>
2019-12-05 15:36:48 +1100

    removed old projects

--- naive_solution/input_reader.h
+++
@@ -1,5 +0,0 @@
-#include "structs.h";
-
-
-vector<Segment> read_segments_from_file();
-vector<Event> events_from_segments(vector<Segment>& segments);
--- naive_solution/naive_algorithm.h
+++
@@ -1,4 +0,0 @@
-#include "input_reader.h";
-
-void process_event(vector<Event>& active_events, Event& new_event);
-vector<Intersection> compute_intersections_naive(vector<Event> events);
